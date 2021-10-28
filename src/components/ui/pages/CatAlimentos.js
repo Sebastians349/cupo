cd888ffabd005429ec711b903d0a6ceff6c35c26
Sebastian Simarro <ssebastian210@gmail.com>
2021-10-28 11:39:17 -0300

    <fix><logica>muy falopa pero anda. Mejorar

--- src/components/ui/pages/CatAlimentos.js
+++ src/components/ui/pages/CatAlimentos.js
@@ -8,8 +8,53 @@ import {
 import { Container } from '@chakra-ui/layout';
 import LogoHeader from '../LogoHeader';
 import PruebaBox from '../PruebaBox';
+// import Negocios from '../../data/Negocios';
 
 const CatAlimentos = () => {
+  const data = [
+    {
+      id: 1,
+      logo: '/assets/img/1.png',
+      nombre: 'local 1',
+      description: 'verduleria',
+      categoria: 'alimentos',
+      oferta: '20 % off',
+      ubicacion: '20 % off',
+      mail: '20 % off',
+      fb: '20 % off',
+      ig: '20 % off',
+      tw: '20 % off',
+      wapp: '26125615', // plan!
+    },
+    {
+      id: 2,
+      logo: '/assets/img/2.png',
+      nombre: 'local 2',
+      description: 'alimentos',
+      categoria: 'panaderia',
+      oferta: '20 % off',
+      ubicacion: '20 % off',
+      mail: '20 % off',
+      fb: '20 % off',
+      ig: '20 % off',
+      tw: '20 % off',
+      wapp: '26125615', // plan!
+    },
+    {
+      id: 3,
+      logo: '/assets/img/3.png',
+      nombre: 'local 3',
+      description: 'peluqueria',
+      categoria: 'otros',
+      oferta: '20 % off',
+      ubicacion: '20 % off',
+      mail: '20 % off',
+      fb: '20 % off',
+      ig: '20 % off',
+      tw: '20 % off',
+      wapp: '50500147', // es el del GCBA
+    },
+  ];
   return (
     <Box p={5}>
       <Breadcrumb>
@@ -28,11 +73,14 @@ const CatAlimentos = () => {
       </Center>
       <Container maxW="container.xl">
         <SimpleGrid minChildWidth="175px" spacing="10px" mt={3}>
+          {/* TODO FIX MEJORAR ESTA FORMA FALOPA DE PASAR DATOS. */}
           <PruebaBox
-            nombre="CUPOGHLAN"
-            logo="/logocupogb500.png"
-            wapp="111111111"
+            key={data[0].id}
+            nombre={data[0].nombre}
+            logo={data[0].logo}
+            wapp={data[0].wapp}
           />
+          {/* <PruebaBox id={negocios.id} nombre={negocios}></PruebaBox> */}
           <PruebaBox nombre="CUPOGHLAN" logo="/logocupogb500.png" />
           <PruebaBox nombre="CUPOGHLAN" logo="/logocupogb500.png" />
           <PruebaBox nombre="CUPOGHLAN" logo="/logocupogb500.png" />
